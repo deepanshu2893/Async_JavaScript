10a4f6d45a01cb4d0a7d2a7bb5a3f60880e6c8ce
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-05 21:31:13 +0530

    callstack

--- Callstack/app.js
+++ Callstack/app.js
@@ -2,4 +2,12 @@ const multiply = (x, y) => x * y;
 const square = x => multiply(x, y);
 
 const isRightTraingle = (a, b, c) =>
-    square(a) + square(b) === square(c); 
+    square(a) + square(b) === square(c);
+
+
+console.log("This is the start");
+
+setTimeout(() => {
+    console.log("I print after 3 seconds");
+}, 3000);
+console.log("I am at the end");
